e060eb8782eb8fcc8f0bafb06b60b28872317cb3
Priyanka <John@192.168.2.119>
2023-11-15 14:26:55 -0500

    Commit

---
+++ src/main/java/Exercise/Ex1_1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercise;
+
+/**
+ *
+ * @author Priyanka
+ */
+public class Ex1_1 {
+    
+    public static void main(String args[]){
+    
+        System.out.println(" Welcome to Java \n Welcome to Computer Science \n Programming is fun");
+    }
+    
+}
